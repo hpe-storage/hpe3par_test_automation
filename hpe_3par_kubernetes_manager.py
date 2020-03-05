3d6ecf2522c6ca154116ebd70944abf0f40a95ef
root <root@k8s-master00.in.rdlabs.hpecorp.net>
2020-03-05 19:45:20 +0530

    Added scripts for clone and publish testing

--- hpe_3par_kubernetes_manager.py
+++ hpe_3par_kubernetes_manager.py
@@ -2,6 +2,7 @@ import yaml
 from kubernetes import client, config
 from time import sleep
 from hpe3parclient.client import HPE3ParClient
+import paramiko
 
 config.load_kube_config()
 k8s_storage_v1 = client.StorageV1Api()
@@ -21,7 +22,12 @@ def hpe_create_sc_object(yml):
 
 def hpe_create_secret_object(yml):
     try:
-        resp = obj = k8s_core_v1.create_namespaced_secret(namespace="kube-system", body=yml)
+        namespace = "default"
+        if 'namespace' in yml.get('metadata'):
+            namespace = yml.get('metadata')['namespace']
+            print("In if :: %s " % namespace)
+        print("Out of if :: %s " % namespace)
+        resp = obj = k8s_core_v1.create_namespaced_secret(namespace=namespace, body=yml)
         print("Storage Class created. status=%s" % resp.metadata.name)
         return resp
     except client.rest.ApiException as e:
@@ -30,34 +36,48 @@ def hpe_create_secret_object(yml):
 
 def hpe_create_pvc_object(yml):
     try:
-        pvc = k8s_core_v1.create_namespaced_persistent_volume_claim(namespace="default", body=yml)
+        namespace = "default"
+        if 'namespace' in yml.get('metadata'):
+            namespace = yml.get('metadata')['namespace']
+        pvc = k8s_core_v1.create_namespaced_persistent_volume_claim(namespace=namespace, body=yml)
         # print("PVC created. status=%s" % pvc.status.phase)
         return pvc
     except client.rest.ApiException as e:
          print("Exception :: %s" % e)
          raise e
 
-
-def hpe_read_pvc_object(pvc_name):
+def hpe_create_pod_object(yml):
     try:
-        pvc = k8s_core_v1.read_namespaced_persistent_volume_claim(pvc_name, namespace="default")
+         namespace = "default"
+         if 'namespace' in yml.get('metadata'):
+            namespace = yml.get('metadata')['namespace']
+         pod = k8s_core_v1.create_namespaced_pod(namespace=namespace, body=yml)
+         return pod
+    except client.rest.ApiException as e:
+          print("Exception :: %s" % e)
+          raise e
+
+
+def hpe_read_pvc_object(pvc_name, namespace="default"):
+    try:
+        pvc = k8s_core_v1.read_namespaced_persistent_volume_claim(pvc_name, namespace=namespace)
         return pvc
     except client.rest.ApiException as e:
         print("Exception :: %s" % e)
         raise e
 
 
-def hpe_delete_pvc_object_by_name(pvc_name):
+def hpe_delete_pvc_object_by_name(pvc_name, namespace="default"):
     try:
-        pvc = k8s_core_v1.delete_namespaced_persistent_volume_claim(pvc_name, namespace="default")
+        pvc = k8s_core_v1.delete_namespaced_persistent_volume_claim(pvc_name, namespace=namespace)
     except client.rest.ApiException as e:
         print("Exception while deleting pvc:: %s" % e)
         raise e
 
 
-def hpe_delete_secret_object_by_name(secret_name):
+def hpe_delete_secret_object_by_name(secret_name, namespace="default"):
     try:
-        pvc = k8s_core_v1.delete_namespaced_secret(secret_name, namespace="kube-system")
+        pvc = k8s_core_v1.delete_namespaced_secret(secret_name, namespace=namespace)
     except client.rest.ApiException as e:
         print("Exception while deleting secret:: %s" % e)
         raise e
@@ -70,19 +90,26 @@ def hpe_delete_sc_object_by_name(sc_name):
         print("Exception while deleting sc:: %s" % e)
         raise e
 
-
-def hpe_list_pvc_objects():
+def hpe_delete_pod_object_by_name(pod_name, namespace = "default"):
     try:
-        pvc_list = k8s_core_v1.list_namespaced_persistent_volume_claim(namespace="default")
+        pvc = k8s_core_v1.delete_namespaced_pod(pod_name, namespace=namespace)
+    except client.rest.ApiException as e:
+        print("Exception while deleting sc:: %s" % e)
+        raise e
+
+
+def hpe_list_pvc_objects(namespace="Default"):
+    try:
+        pvc_list = k8s_core_v1.list_namespaced_persistent_volume_claim(namespace=namespace)
         return pvc_list
     except client.rest.ApiException as e:
         print("Exception :: %s" % e)
         raise e
 
-def hpe_list_pvc_objects_names():
+def hpe_list_pvc_objects_names(namespace="Default"):
     try:
         pvc_names = []
-        pvc_list = hpe_list_pvc_objects()
+        pvc_list = hpe_list_pvc_objects(namespace)
         for pvc in pvc_list.items:
             pvc_names.append(pvc.metadata.name)
 
@@ -100,6 +127,7 @@ def hpe_list_sc_objects():
         print("Exception :: %s" % e)
         raise e
 
+
 def hpe_list_sc_objects_names():
     try:
         sc_names = []
@@ -113,18 +141,19 @@ def hpe_list_sc_objects_names():
         raise e
 
 
-def hpe_list_secret_objects():
+def hpe_list_secret_objects(namespace="Default"):
     try:
-        secret_list = k8s_core_v1.list_namespaced_secret(namespace="kube-system")
+        secret_list = k8s_core_v1.list_namespaced_secret(namespace=namespace)
         return secret_list
     except client.rest.ApiException as e:
         print("Exception :: %s" % e)
         raise e
 
-def hpe_list_secret_objects_names():
+
+def hpe_list_secret_objects_names(namespace="Default"):
     try:
         secret_names = []
-        secret_list = hpe_list_secret_objects()
+        secret_list = hpe_list_secret_objects(namespace=namespace)
         for secret in secret_list.items:
             secret_names.append(secret.metadata.name)
 
@@ -134,7 +163,32 @@ def hpe_list_secret_objects_names():
         raise e
 
 
-def check_if_deleted(timeout, name, kind):
+def hpe_list_pod_objects(namespace="Default", **kwargs):
+    try:
+        print("kwargs :: keys :: %s,\n values :: %s" % (kwargs.keys(), kwargs.values()))
+        # print("value :: %s " % kwargs['label'])
+        # pod_list = k8s_core_v1.list_namespaced_pod(namespace=namespace, label_selector=kwargs['label'])
+        pod_list = k8s_core_v1.list_namespaced_pod(namespace=namespace)
+        return pod_list
+    except client.rest.ApiException as e:
+        print("Exception :: %s" % e)
+        raise e
+
+
+def hpe_list_pod_objects_names(namespace="Default"):
+    try:
+        pod_names = []
+        pod_list = hpe_list_pod_objects(namespace=namespace)
+        for pod in pod_list.items:
+            pod_names.append(pod.metadata.name)
+
+        return pod_names
+    except client.rest.ApiException as e:
+        print("Exception :: %s" % e)
+        raise e
+
+
+def check_if_deleted(timeout, name, kind, namespace="Default"):
     time = 0
     flag = True
 
@@ -144,17 +198,19 @@ def check_if_deleted(timeout, name, kind):
 
     while True:
         if kind == 'PVC':
-            obj_list = hpe_list_pvc_objects_names()
+            obj_list = hpe_list_pvc_objects_names(namespace=namespace)
         elif kind == 'SC':
             obj_list = hpe_list_sc_objects_names()
         elif kind == 'Secret':
-            obj_list = hpe_list_secret_objects_names()
+            obj_list = hpe_list_secret_objects_names(namespace=namespace)
+        elif kind == 'Pod':
+            obj_list = hpe_list_pod_objects_names(namespace=namespace)
         else:
             print("Not a supported kind")
             flag = False
             break
         # print(obj_list)
-        print".",
+        print(".", end='', flush=True)
         if name not in obj_list:
             break
 
@@ -192,3 +248,93 @@ def _hpe_get_3par_client_login(HPE3PAR_API_URL, HPE3PAR_IP, HPE3PAR_USERNAME, HP
     hpe_3par_cli.login(HPE3PAR_USERNAME, HPE3PAR_PWD)
     hpe_3par_cli.setSSHOptions(HPE3PAR_IP, HPE3PAR_USERNAME, HPE3PAR_PWD)
     return hpe_3par_cli
+
+def check_status(timeout, name, kind, status, namespace="default"):
+    time = 0
+    flag = True
+    # print("timeout :: %s" % timeout)
+    # print("time :: %s" % time)
+    # print("kind :: %s" % kind)
+    # print("status :: %s" % status)
+
+    while True:
+        obj = ""
+        if kind == 'pv':
+            obj = k8s_core_v1.read_persistent_volume(name)
+        elif kind == 'pvc':
+            obj = k8s_core_v1.read_namespaced_persistent_volume_claim(name, namespace)
+        elif kind == 'pod':
+            obj = k8s_core_v1.read_namespaced_pod(name, namespace)
+        elif kind == 'deployment':
+            obj = k8s_apps_v1.read_namespaced_deployment(name, namespace)
+            print(obj)
+        else:
+            print("Not a supported kind")
+            flag = False
+            break
+        # print("obj.status.phase :: %s" % obj.status.phase)
+        print(".", end='', flush=True)
+        if kind == 'deployment':
+            replica_total = 0
+            replica_avail = 0
+
+            if obj.status.replicas is not None:
+                replica_total = obj.status.replicas
+
+            if obj.status.available_replicas is not None:
+                replica_avail = obj.status.available_replicas
+            if replica_total == replica_avail:
+                break
+        else:
+            if obj.status.phase == status:
+                break
+
+        if int(time) > int(timeout):
+            print("\n%s not yet in %s state. Taking longer than expected..." % (kind, status))
+            # print("obj :: %s" % obj)
+            flag = False
+            break
+        time += 1
+        sleep(1)
+
+    return flag,obj
+
+def hpe_get_pod(pod_name, namespace = "default"):
+    try:
+        return k8s_core_v1.read_namespaced_pod(pod_name,namespace=namespace)
+    except Exception as e:
+        print("Exception :: %s" % e)
+        raise e
+
+def get_command_output(node_name, command):
+    try:
+        # print("Executing command...")
+        ssh_client = paramiko.SSHClient()
+        # print("ssh client %s " % ssh_client)
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # print("host key set")
+        # print("node_name = %s, command = %s " % (node_name, command))
+        ssh_client.connect(hostname=node_name)
+        # ssh_client.connect(node_name, username='vagrant', password='vagrant', key_filename='/home/vagrant/.ssh/id_rsa')
+        # ssh_client.connect(node_name, username='vagrant', password='vagrant', look_for_keys=False, allow_agent=False)
+        # print("connected...")
+        # execute command and get output
+        stdin,stdout,stderr=ssh_client.exec_command(command)
+        # print("stdout :: %s " % stdout.read())
+        command_output = []
+        while True:
+            line = stdout.readline()
+            if not line:
+                break
+            command_output.append(str(line).strip())
+            print(line)
+        # command_output = stdout.read()
+
+        # print("stdin :: " % stdin.readlines())
+        # print("stderr :: %s" % stderr.read())
+        ssh_client.close()
+
+        return command_output
+    except Exception as e:
+        print("Exception while ssh %s " % e)
+
--- test_clone.py
+++ test_clone.py
@@ -14,12 +14,12 @@ HPE3PAR_API_URL = None
 
 hpe3par_cli = None
 
+
 def test_create_objects():
     print("\n########################### test_create_objects ###########################")
     try:
         # Create PV, PVC and Pod
-        yml = "YAML/test-pvc-crud.yml"
-        global pv_name, pvc_name, pod_name, pod_node_name
+        yml = "YAML//base-pvc-clone.yml"
         with open(yml) as f:
             elements = list(yaml.safe_load_all(f))
             for el in elements:
@@ -50,23 +50,24 @@ def test_create_objects():
 def test_verify_pvc():
     print("\n########################### test_verify_pvc ###########################")
     try:
-        print("Sleeping for 30 seconds...")
-        sleep(30)
-        print("Resume")
-        PVC = manager.hpe_read_pvc_object(objects['PVC'])
-        assert PVC.status.phase == 'Bound', 'PVC %s is not in Bound state ' % objects['PVC']
-        print("PVC %s is in Bound state" % objects['PVC'])
-        objects["PVC_Volume_name"] = str(PVC.spec.volume_name)[0:31]
+        print("Waiting for PVC %s to come in Bound state..." % objects['PVC'])
+        flag, pvc_obj = manager.check_status(timeout, objects['PVC'], kind='pvc', status='Bound')
+        if flag is True:
+            assert flag is True, \
+                "Persistent Volume %s status check timed out, not in Bound state yet..." % objects['PVC']
+        print("\nPVC %s is in Bound state" % objects['PVC'])
+        objects['PVC_Volume_name'] = str(pvc_obj.spec.volume_name)[0:31]
     except Exception as e:
         print("Exception while verifying pvc status :: %s" % e)
         raise e
 
 
 def test_verify_on_3par():
+    print("\n########################### test_verify_on_3par ###########################")
     try:
         global HPE3PAR_IP, HPE3PAR_USERNAME, HPE3PAR_PWD, HPE3PAR_API_URL
         global hpe3par_cli
-        HPE3PAR_IP, HPE3PAR_USERNAME, HPE3PAR_PWD = manager.read_array_prop("YAML/test-pvc-crud.yml")
+        HPE3PAR_IP, HPE3PAR_USERNAME, HPE3PAR_PWD = manager.read_array_prop("YAML/base-pvc-clone.yml")
         HPE3PAR_API_URL = "https://" + HPE3PAR_IP + ":8080/api/v1"
         print("HPE3PAR_API_URL :: %s, HPE3PAR_IP :: %s, HPE3PAR_USERNAME :: %s, HPE3PAR_PWD :: %s" % (HPE3PAR_API_URL,
                                                                                                       HPE3PAR_IP,
@@ -76,6 +77,7 @@ def test_verify_on_3par():
         hpe3par_cli = manager._hpe_get_3par_client_login(HPE3PAR_API_URL, HPE3PAR_IP,
                                                          HPE3PAR_USERNAME, (base64.b64decode(HPE3PAR_PWD)).decode(encoding))
         hpe3par_volume = hpe3par_cli.getVolume(objects["PVC_Volume_name"])
+        hpe3par_cli.logout()
         assert hpe3par_volume is not None, "Volume is not created on 3PAR for pvc %s " % objects['PVC']
         print("\n Volume is created successfully on 3PAR for PVC %s " % objects['PVC'])
     except Exception as e:
@@ -83,8 +85,61 @@ def test_verify_on_3par():
         raise e
 
 
-def test_delete_pvc():
-    print("\n########################### test_delete_pvc ###########################")
+def test_create_clone_pvc():
+    print("\n########################### test_create_clone_pvc ###########################")
+    try:
+        yml = "YAML/test-clone.yml"
+        with open(yml) as f:
+            elements = list(yaml.safe_load_all(f))
+            for el in elements:
+                if str(el.get('kind')) == "PersistentVolumeClaim":
+                    print("\nCreating PVC...")
+                    pvc_name = manager.hpe_create_pvc_object(el).metadata.name
+                    print("\nPersistentVolumeClaim %s created." % pvc_name)
+                    objects["PVC-clone"] = str(pvc_name)
+        sleep(10)
+        PVC = manager.hpe_read_pvc_object(objects['PVC'])
+        assert PVC is not None, 'Clone PVC %s is not created.' % objects["PVC-clone"]
+    except Exception as e:
+        print("Exception while creating clone pvc :: %s" % e)
+        raise e
+    # Now verify if it comes to Bound
+    try:
+        print("Waiting for PVC %s to come in Bound state..." % objects['PVC-clone'])
+        flag, pvc_obj = manager.check_status(timeout, objects['PVC-clone'], kind='pvc', status='Bound')
+        if flag is True:
+            assert flag is True, \
+                "Persistent Volume %s status check timed out, not in Bound state yet..." % objects['PVC-clone']
+        objects['PVC-clone_Volume_name'] = str(pvc_obj.spec.volume_name)[0:31]
+        assert PVC.status.phase == 'Bound', 'Clone PVC %s is not in Bound state ' % objects['PVC-clone']
+        print("Clone PVC %s is in Bound state" % objects['PVC-clone'])
+    except Exception as e:
+        print("Exception while verifying clone pvc Bound status :: %s" % e)
+        raise e
+
+
+@pytest.mark.skip(reason="skipped as not implementation yet")
+def test_verify_pvc_properties():
+    print("\n########################### test_create_clone_pvc ###########################")
+    # to be implemented
+
+
+def test_delete_clone():
+    print("\n########################### test_delete_clone ###########################")
+    try:
+        name = objects["PVC-clone"]
+        print("Deleting clone PVC %s..." % name)
+        manager.hpe_delete_pvc_object_by_name(name)
+
+        assert manager.check_if_deleted(timeout, name, "PVC") is True, "Clone PVC %s is not deleted yet " % name
+        print("Clone PVC %s is deleted." % name)
+    except Exception as e:
+        print("Exception while deleting clone pvc :: %s" % e)
+        raise e
+
+
+def test_delete_base_pvc():
+    print("\n########################### test_delete_base_pvc ###########################")
     try:
         name = objects["PVC"]
         print("Deleting PVC %s..." % name)
@@ -93,7 +148,7 @@ def test_delete_pvc():
         assert manager.check_if_deleted(timeout, name, "PVC") is True, "PVC %s is not deleted yet " % name
         print("PVC %s is deleted." % name)
     except Exception as e:
-        print("Exception while deleting pvc :: %s" % e)
+        print("Exception while deleting base pvc :: %s" % e)
         raise e
 
 
@@ -122,19 +177,26 @@ def test_delete_secret():
         assert manager.check_if_deleted(timeout, name, "Secret", namespace=namespace) is True, \
             "Secret %s is not deleted yet " % name
         print("Secret %s is deleted." % name)
-
     except Exception as e:
         print("Exception while deleting secret :: %s" % e)
         raise e
 
 
-def test_verify_delete_on_3par():
+def test_verify_delete_clone_pvc_on_3par():
     try:
-        global hpe3par_cli
+        hpe3par_volume = hpe3par_cli.getVolume(objects["PVC-clone_Volume_name"])
+        assert hpe3par_volume is None, "Clone Volume is not deleted from 3PAR after deleting of clone pvc %s " % objects['PVC-clone']
+    except Exception as e:
+        print("Exception while verifying clone volume deletion on 3par :: %s" % e)
+        raise e
+
+
+def test_verify_delete_base_pvc_on_3par():
+    try:
         hpe3par_volume = hpe3par_cli.getVolume(objects["PVC_Volume_name"])
         hpe3par_cli.logout()
-        assert hpe3par_volume is None, "Volume is not deleted from 3PAR after deleting of pvc %s " % objects['PVC']
+        assert hpe3par_volume is None, "Volume is not deleted from 3PAR after deleting of base pvc %s " % objects['PVC']
     except Exception as e:
-        print("Exception while verifying on 3par :: %s" % e)
+        print("Exception while verifying base volume deletion on 3par :: %s" % e)
         raise e
 
